0eb9dc0244bb50b02ddf4de82c826aef69617492
neosarchizo <neosarchizo@gmail.com>
2022-05-08 13:24:00 +0900

    feat: add interrupt

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from machine import Pin
+from time import sleep
 
 led = Pin(25, Pin.OUT)
 key = Pin(14, Pin.IN, Pin.PULL_UP)
 
-while True:
-    if not key.value(): # pressed, 0, LOW
-        led.high()
-    else: # released, 1, HIGH
-        led.low()
+state = 0
+
+def on_falling(k):
+    global state
+    sleep(0.01)
+
+    if not k.value(): # pressed, 0, LOW
+        state = not state
+        led.value(state)
+
+key.irq(on_falling, Pin.IRQ_FALLING)
