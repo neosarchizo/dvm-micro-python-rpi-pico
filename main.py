20e7df6c80283f90462b1a8f83944c2c8227deb5
neosarchizo <neosarchizo@gmail.com>
2022-05-09 15:57:27 +0900

    feat: add Timer

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from machine import Pin
-from time import sleep
+from machine import Pin, Timer
 
 led = Pin(25, Pin.OUT)
-key = Pin(14, Pin.IN, Pin.PULL_UP)
 
-state = 0
+def timeout(t):
+    led.toggle()
 
-def on_falling(k):
-    global state
-    sleep(0.01)
-
-    if not k.value(): # pressed, 0, LOW
-        state = not state
-        led.value(state)
-
-key.irq(on_falling, Pin.IRQ_FALLING)
+timer = Timer()
+timer.init(
+    period = 1000, # ms
+    mode = Timer.PERIODIC, # ONE_SHOT
+    callback = timeout
+)
