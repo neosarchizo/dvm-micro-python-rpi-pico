2c00de14b51ba41ddffe313e1c71f26d16aad7ab
neosarchizo <neosarchizo@gmail.com>
2022-05-15 14:42:46 +0900

    feat: add dht11

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from machine import Pin, SoftI2C
 from ssd1306 import SSD1306_I2C
-from onewire import OneWire
-from ds18x20 import DS18X20
+from dht import DHT11
 from time import sleep
 
-i2c = SoftI2C(scl = Pin(10), sda = Pin(11))
-display = SSD1306_I2C(128, 64, i2c, addr = 0x3C)
+i2c = SoftI2C(scl=Pin(10), sda=Pin(11))
+display = SSD1306_I2C(128, 64, i2c, addr=0x3C)
 
-ds = DS18X20(OneWire(Pin(16)))
-rom = ds.scan()
+sleep(1)
+
+dht = DHT11(Pin(17))
 
 while True:
-    ds.convert_temp()
-    temp = ds.read_temp(rom[0])
+    dht.measure()
+
+    temperature = dht.temperature()
+    humidity = dht.humidity()
 
     display.fill(0)
     display.text('DeviceMart', 0, 0)
-    display.text('Temperature', 0, 20)
-    display.text(str('%.2f' % temp) + ' C', 0, 40)
+    display.text('DHT11', 0, 15)
+    display.text(str(temperature) + ' C', 0, 40)
+    display.text(str(humidity) + ' %', 55, 40)
     display.show()
 
-    sleep(1)
+    print(str(temperature) + ' C')
+    print(str(humidity) + ' %')
+
+    sleep(2)
