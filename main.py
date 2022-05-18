08ffda96676694ec9b134b93d1583055435e75bb
neosarchizo <neosarchizo@gmail.com>
2022-05-18 12:50:39 +0900

    feat: add soil

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ from time import sleep
 i2c = SoftI2C(scl=Pin(10), sda=Pin(11))
 display = SSD1306_I2C(128, 64, i2c, addr=0x3C)
 
-light = ADC(1)
+soil = ADC(1)
 
 while True:
     display.fill(0)
     display.text('DeviceMart', 0, 0)
-    display.text('Light', 0, 15)
+    display.text('Soil', 0, 15)
 
-    value = light.read_u16() # 2 ^ 16, 65535
+    value = soil.read_u16() # 2 ^ 16, 65535
 
-    display.text(str(value) + ' / 65535', 0, 40)
-    display.text(str('%.2f' % ((value / 65535) * 3.3)) + ' V', 0, 55)
+    display.text(str(value) + ' / 65535', 0, 40) # 2 ^ 16
+    display.text(str(round(value * 3.3 / 65535, 2)) + ' V', 0, 55)
 
-    if value <= 21840:
-        display.text('Bright', 60, 55)
-    elif value <= 43680:
+    if value <= 19952:
+        display.text('Dry', 60, 55)
+    elif value <= 35808:
         display.text('Normal', 60, 55)
     else:
-        display.text('Weak', 60, 55)
+        display.text('Wet', 60, 55)
     
     display.show()
     sleep(1)
