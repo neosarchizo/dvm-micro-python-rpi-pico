064c9d033745ee4c35a9f2bc629145441abf010e
neosarchizo <neosarchizo@gmail.com>
2022-05-22 14:42:01 +0900

    feat: add flash animation

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from ws2812b import WS2812B
 from time import sleep
 
-pixels = WS2812B(30, 27) # pixel count, pin number Y11
+NUM_LEDS = 30
+pixels = WS2812B(num_leds=NUM_LEDS, pin=27, delay=0)
+
+pixels.fill(10, 10, 10)
+pixels.show()
 
 while True:
-    pixels.fill(255, 0, 0)
-    pixels.show()
-    sleep(1)
-    
-    pixels.fill(0, 255, 0)
-    pixels.show()
-    sleep(1)
-
-    pixels.fill(0, 0, 255)
-    pixels.show()
-    sleep(1)
+    for i in range(NUM_LEDS):
+        for j in range(NUM_LEDS):
+            pixels.set_pixel(j, abs(i + j) % 10, abs(i - (j + 3)) % 10, abs(i - (j + 6)) % 10)
+        pixels.show()
+        sleep(0.05)
