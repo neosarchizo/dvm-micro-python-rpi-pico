4fb7230e3a5ee25410ba896c66b4ca0a3ade412a
neosarchizo <neosarchizo@gmail.com>
2022-05-24 15:52:45 +0900

    feat: add firefly animation

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
 from ws2812b import WS2812B
 from time import sleep
+from random import randint
 
 NUM_LEDS = 30
+MIN_LEN = 5
+MAX_LEN = 20
+NUM_FLASHES = 10
+
 pixels = WS2812B(NUM_LEDS, 27)
 
-RED = (255, 0, 0) #ff0000
-ORANGE = (255, 165, 0) #ffa500
-YELLOW = (255, 150, 0) #ff9500
-GREEN = (0, 255, 0) #00ff00
-BLUE = (0, 0, 255) #0000ff
-INDIGO = (75, 0, 130) #4c0082
-VIOLET = (138, 43, 226) #8a2be2
-
 COLORS = [
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    BLUE,
-    INDIGO,
-    VIOLET,
+    [232, 100, 255],
+    [200, 200, 20],
+    [30, 200, 200],
+    [150, 50, 10],
+    [50, 200, 10],
 ]
 
+flashing = []
+
+for i in range(NUM_FLASHES):
+    pos = randint(0, NUM_LEDS - 1) # 0 ~ 29
+    idxColor = randint(0, len(COLORS) - 1) # 0 ~ 4
+    flashLength = randint(MIN_LEN, MAX_LEN) # 5 ~ 20
+    flashing.append([pos, COLORS[idxColor], flashLength, 0, 1]) # pixel position, color, flash length, current index of flash length, step direction
+
+pixels.fill(0, 0, 0)
+
 while True:
-    for color in COLORS:
-        for i in range(NUM_LEDS):
-            pixels.set_pixel(i, color[0], color[1], color[2])
-            sleep(0.01)
-            pixels.show()
+    pixels.show()
+
+    for i in range(NUM_FLASHES):
+        p = flashing[i][0] # pixel position
+        brightness = (flashing[i][3] / flashing[i][2]) # current index of flash length / flash length
+        color = (
+            int(flashing[i][1][0] * brightness),
+            int(flashing[i][1][1] * brightness),
+            int(flashing[i][1][2] * brightness)
+        )
+        pixels.set_pixel(p, color[0], color[1], color[2])
+
+        if flashing[i][2] == flashing[i][3]: # maximum brightness ??
+            flashing[i][4] = -1 # step direction
+
+        if flashing[i][3] == 0 and flashing[i][4] == -1: # end
+            pos = randint(0, NUM_LEDS - 1) # 0 ~ 29
+            idxColor = randint(0, len(COLORS) - 1) # 0 ~ 4
+            flashLength = randint(MIN_LEN, MAX_LEN) # 5 ~ 20
+            flashing[i] = [pos, COLORS[idxColor], flashLength, 0, 1] # update
+
+        flashing[i][3] = flashing[i][3] + flashing[i][4]
+        sleep(0.005)
