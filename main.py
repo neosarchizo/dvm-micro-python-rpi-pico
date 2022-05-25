900d522164a48a55a5b72a2e62a273f9d06a2cd9
neosarchizo <neosarchizo@gmail.com>
2022-05-25 14:48:28 +0900

    feat: add st7735

--- main.py
+++ main.py
@@ -1,53 +1,23 @@
-from ws2812b import WS2812B
+import lcd_gfx
+from machine import SPI
+from st7735 import ST7735
 from time import sleep
-from random import randint
+from bmp import BMP
 
-NUM_LEDS = 30
-MIN_LEN = 5
-MAX_LEN = 20
-NUM_FLASHES = 10
-
-pixels = WS2812B(NUM_LEDS, 27)
-
-COLORS = [
-    [232, 100, 255],
-    [200, 200, 20],
-    [30, 200, 200],
-    [150, 50, 10],
-    [50, 200, 10],
-]
-
-flashing = []
-
-for i in range(NUM_FLASHES):
-    pos = randint(0, NUM_LEDS - 1) # 0 ~ 29
-    idxColor = randint(0, len(COLORS) - 1) # 0 ~ 4
-    flashLength = randint(MIN_LEN, MAX_LEN) # 5 ~ 20
-    flashing.append([pos, COLORS[idxColor], flashLength, 0, 1]) # pixel position, color, flash length, current index of flash length, step direction
-
-pixels.fill(0, 0, 0)
+spi = SPI(1, baudrate=8000000, polarity=0, phase=0)
+display = ST7735(spi, rst=13, ce=9, dc=12)
+display.reset()
+display.begin()
 
 while True:
-    pixels.show()
+    display._bground = 0xffff
+    display.fill_screen(display._bground)
+    display.pixel(5, 5, display.rgb_to_565(0, 255, 0))
+    lcd_gfx.drawLine(5, 10, 80, 10, display, display.rgb_to_565(0, 0, 255))
+    lcd_gfx.drawRect(5, 20, 80, 40, display, display.rgb_to_565(255, 255, 0))
+    lcd_gfx.drawCircle(40, 90, 20, display, display.rgb_to_565(0, 255, 255))
+    display.p_string(10, 130, "Hello DeviceMart!", display.rgb_to_565(255, 0, 0))
+    sleep(3)
 
-    for i in range(NUM_FLASHES):
-        p = flashing[i][0] # pixel position
-        brightness = (flashing[i][3] / flashing[i][2]) # current index of flash length / flash length
-        color = (
-            int(flashing[i][1][0] * brightness),
-            int(flashing[i][1][1] * brightness),
-            int(flashing[i][1][2] * brightness)
-        )
-        pixels.set_pixel(p, color[0], color[1], color[2])
-
-        if flashing[i][2] == flashing[i][3]: # maximum brightness ??
-            flashing[i][4] = -1 # step direction
-
-        if flashing[i][3] == 0 and flashing[i][4] == -1: # end
-            pos = randint(0, NUM_LEDS - 1) # 0 ~ 29
-            idxColor = randint(0, len(COLORS) - 1) # 0 ~ 4
-            flashLength = randint(MIN_LEN, MAX_LEN) # 5 ~ 20
-            flashing[i] = [pos, COLORS[idxColor], flashLength, 0, 1] # update
-
-        flashing[i][3] = flashing[i][3] + flashing[i][4]
-        sleep(0.005)
+    BMP('logo.bmp', display, 0, 0, 1)
+    sleep(3)
